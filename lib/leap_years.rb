5963d0061a51732fd91bdcd1f1fd70d9ce552cae
Patricia <prmonday@gmail.com>
2020-11-17 22:08:45 +0000

    3 test case passed

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,5 +1,7 @@
 def leap_years(input_year)
     if input_year % 400 == 0
       return true
+    else
+      return false 
     end
 end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -7,4 +7,7 @@ require 'leap_years'
    it 'takes a year that is divisible by 400 and return true'do
      expect(leap_years(1600)).to eq(true)
    end
+   it 'takes a year that is divisible by 100 and not by 400 and return'do
+     expect(leap_years(1700)).to eq(false)
+   end
  end
