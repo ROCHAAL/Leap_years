6bda8fac7089a1913c166d7180aedbca05af31eb
Patricia <prmonday@gmail.com>
2020-11-18 11:47:54 +0000

    4 cases passed

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,7 +1,26 @@
 def leap_years(input_year)
+
     if input_year % 400 == 0
-      return true
+        true
+    elsif input_year % 100 == 0
+        false
+    elsif input_year % 4 == 0
+        true
     else
-      return false 
+        false
     end
+
 end
+
+
+
+# def leap_years(input_year)
+#     if input_year % 400 == 0 || input_year % 4 == 0 (Why the || does not work?)
+#       true
+#     elsif  input_year % 100 == 0
+#       return false
+#     else
+#       false
+#
+#     end
+# end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -7,7 +7,10 @@ require 'leap_years'
    it 'takes a year that is divisible by 400 and return true'do
      expect(leap_years(1600)).to eq(true)
    end
-   it 'takes a year that is divisible by 100 and not by 400 and return'do
+   it 'takes a year that is divisible by 100 and not by 400 and return false'do
      expect(leap_years(1700)).to eq(false)
    end
+   it 'takes a year that is divisible by 4 and not by 100 and return true'do
+   expect(leap_years(2004)).to eq(true)
+  end
  end
