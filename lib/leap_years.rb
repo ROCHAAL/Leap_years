ef3976d118524c753a6c8b82e82b9e8314c20413
Patricia <prmonday@gmail.com>
2021-04-26 12:43:15 +0100

    test passed

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,26 +1,30 @@
-def leap_years(input_year)
+# class LeapYears
+#
+#   def leap_year?(year)
+#       if year % 400 == 0
+#          true
+#       elsif year % 400 != 0
+#        false
+#       elsif year % 4 == 0
+#        true
+#       else
+#         false
+#       end
+#   end
+# end
 
-    if input_year % 400 == 0
+class LeapYears
+def leap_year?(year)
+
+    if year % 400 == 0
         true
-    elsif input_year % 100 == 0
+    elsif year % 100 == 0
         false
-    elsif input_year % 4 == 0
+    elsif year % 4 == 0
         true
     else
         false
     end
 
 end
-
-
-
-# def leap_years(input_year)
-#     if input_year % 400 == 0 || input_year % 4 == 0 (Why the || does not work?)
-#       true
-#     elsif  input_year % 100 == 0
-#       return false
-#     else
-#       false
-#
-#     end
-# end
+end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -1,16 +1,34 @@
 require 'leap_years'
 
- describe 'leap_years'do
-   it 'takes a year that is divisible by 400 and return true'do
-     expect(leap_years(2000)).to eq(true)
-   end
-   it 'takes a year that is divisible by 400 and return true'do
-     expect(leap_years(1600)).to eq(true)
-   end
-   it 'takes a year that is divisible by 100 and not by 400 and return false'do
-     expect(leap_years(1700)).to eq(false)
-   end
-   it 'takes a year that is divisible by 4 and not by 100 and return true'do
-   expect(leap_years(2004)).to eq(true)
-  end
+describe 'LeapYears' do
+  it' shows that all years divisible by 400 are leap years' do
+   leap_years = LeapYears.new
+   expect(leap_years.leap_year?(2000)).to eq(true)
  end
+
+ it ' takes the years divided by 100 and not by 400' do
+   leap_years = LeapYears.new
+   expect(leap_years.leap_year?(1700)).to eq(false)
+ end
+ it 'shows all years divisible by 4 are leap years'do
+ leap_year = LeapYears.new
+ expect(leap_year.leap_year?(2004)).to eq(true)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# input               output
+# y 400 % == 0        true
+# y 400 % != 0        false
+# y 4   % == 0        true
+# y 4  % != 0         false
